34567af629ec4f25175b4ebdd5ddf79360b08a99
FIC <fic@info.local>
2025-02-18 21:56:07 -0300

    atualizado

--- sandbox/repeticoes.js
+++ sandbox/repeticoes.js
@@ -1,6 +1,13 @@
+//exercicio 1
+//for
+function exercicio01 () {
 for (let contador = 1; contador <= 10; contador++) {
     console.log("contagem", contador);
+ }
+
 }
+exercicio01();
+
 
 //let contador = 1;
 //while (contador <=5) {
@@ -9,23 +16,61 @@ for (let contador = 1; contador <= 10; contador++) {
 //contador++
 //}
 
-
-//let numero = prompt("Digite um numero para ver sua tabuada:");
-//let resultado;
-
-
-//for (let contador = 1; contador <= 10; contador++) {
-// console.log(`${numero} x ${contador} = ${numero * contador}`);
-//}
-
-let numero = prompt("Digite um numero:");
-
-let soma = 0;
-
-for (let contador = 1; contador <= numero; contador++) {
-    soma += contador;
+function exercicio02 () {
+let numero = prompt("Digite um numero para ver sua tabuada:");
+let resultado;
 }
 
-console.log ("soma dos primeiros " + numero + " números naturais é: " + soma);
-alert("soma dos primeiros " + numero + " números naturais é: " + soma);
+for (let contador = 1; contador <= 10; contador++) {
+console.log(`${numero} x ${contador} = ${numero * contador}`);
+}
+exercicio02();
 
+//exercício 2
+//let numero = prompt("Digite um numero:");
+
+//let soma = 0;
+
+//for (let contador = 1; contador <= numero; contador++) {
+  //  soma += contador;
+//}
+
+//console.log ("soma dos primeiros " + numero + " numeros naturais e: " + soma);
+
+//alert("soma dos primeiros " + numero + " numeros naturais e: " + soma);
+
+//exercício 3
+
+
+//let numeroAleatorio = Math.floor(Math.random() * 100);
+
+//let chute = null;
+
+//while (chute != numeroAleatorio) {
+
+  //  chute = parseInt(prompt("Chute um número:"));
+
+    //if (chute != numeroAleatorio) {
+
+//        if (chute > numeroAleatorio) {
+    
+  //          alert("Chutou alto.");
+    
+    //    } else {
+    
+      //      alert("Chutou baixo.");
+    
+        //}
+
+//     }
+
+// }
+
+//alert("Você acertou!");
+
+// let numero = prompt("Digite um numero para inicia contagem regressiva:");
+
+// while (numero >= 0) {
+//     console.log(numero);
+//     numero--;
+// }
